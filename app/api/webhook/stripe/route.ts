98f7b474fe7a46bb44a15e062352339f8ef662c3
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-06-08 01:34:35 +0700

    Complete setup Stripe and still fix error 401

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,3 +1,59 @@
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+// const stripe = require('stripe')('sk_test_...');
+// const express = require('express');
+// const app = express();
+
+
+// // This is your Stripe CLI webhook secret for testing your endpoint locally.
+// const endpointSecret = "whsec_3f6ad692154e4a606a44fa96110d9b7191372ccd4e760a11dd9bbb517f772e23";
+
+// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//   } catch (err) {
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'checkout.session.completed':
+//       const checkoutSessionCompleted = event.data.object;
+//       // Then define and call a function to handle the event checkout.session.completed
+//       break;
+//     // ... handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+// });
+
+// app.listen(4242, () => console.log('Running on port 4242'));
+
+
+
+
+
 import stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order.action'
@@ -8,6 +64,8 @@ export async function POST(request: Request) {
   const sig = request.headers.get('stripe-signature') as string
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
 
+  console.log(sig)
+
   let event
 
   try {
@@ -15,14 +73,16 @@ export async function POST(request: Request) {
   } catch (err) {
     return NextResponse.json({ message: 'Webhook error', error: err })
   }
-
+  
   // Get the ID and type
   const eventType = event.type
+
+  console.log("Cai dit me", event);
 
   // CREATE
   if (eventType === 'checkout.session.completed') {
     const { id, amount_total, metadata } = event.data.object
-
+    
     const order = {
       stripeId: id,
       eventId: metadata?.eventId || '',
--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -1,41 +1,40 @@
-"use server"
+"use server";
 
-import Stripe from 'stripe'
-import { CheckoutOrderParams, CreateOrderParams } from "@/types"
-import { redirect } from 'next/navigation';
-import { connectToDatabase } from '../database';
-import Order from '../database/models/order.model';
+import Stripe from "stripe";
+import { CheckoutOrderParams, CreateOrderParams } from "@/types";
+import { redirect } from "next/navigation";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
   const price = order.isFree ? 0 : Number(order.price) * 100;
+
   try {
-    
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          // price: '{{PRICE_ID}}',
-          quantity: 1,
           price_data: {
             currency: 'usd',
             unit_amount: price,
             product_data: {
               name: order.eventTitle
             }
-          }
+          },
+          quantity: 1
         },
       ],
       metadata: {
         eventId: order.eventId,
-        buyerId: order.buyerId
+        buyerId: order.buyerId,
       },
       mode: 'payment',
       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
 
-  redirect(session.url!)
+    redirect(session.url!)
   } catch (error) {
     throw error;
   }
@@ -43,16 +42,14 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
 
 export const createOrder = async (order: CreateOrderParams) => {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
     const newOrder = await Order.create({
       ...order,
       event: order.eventId,
-      buyer: order.buyerId
-    })
+      buyer: order.buyerId,
+    });
 
-    return JSON.parse(JSON.stringify(newOrder))
-  } catch (error) {
-    
-  }
-}
+    return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {}
+};
